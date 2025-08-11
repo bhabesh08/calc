11ab4f9a9fa42c9cc33efaefdf4cef1b7d95da98
bhabesh08 <160404286+bhabesh08@users.noreply.github.com>
2025-08-11 22:57:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="Adder", page_icon="➕", layout="left")
 st.title("Add Two Numbers")
 
-# Use a form so the calculation happens only when you click the button
 with st.form("adder_form"):
-    number1 = st.number_input("Number-1", value=0.0, step=1.0, format="%.6f")
-    number2 = st.number_input("Number-2", value=0.0, step=1.0, format="%.6f")
+    number1 = st.number_input("Number-1", value=0.0, step=1.0, format="%.0f")
+    number2 = st.number_input("Number-2", value=0.0, step=1.0, format="%.0f")
     submitted = st.form_submit_button("Calculate")
 
 if submitted:
