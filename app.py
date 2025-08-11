4f6a1e649ea199766ea23f6e0ce55ff1ddc5c4b5
bhabesh08 <160404286+bhabesh08@users.noreply.github.com>
2025-08-11 23:00:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Adder", page_icon="➕", layout="centre")
+st.set_page_config(page_title="Adder", page_icon="➕", layout="centred")
 st.title("Add Two Numbers")
 
 with st.form("adder_form"):
